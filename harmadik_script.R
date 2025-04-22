9bf6e48af8444ff13b8488db4b3dcad79d4da3cf
Ábel <abel.konrad@gmail.com>
2025-04-22 18:19:47 +0200

    Harmadik módosítás: indulásnál késők

---
+++ harmadik_script.R
@@ -0,0 +1,27 @@
+#harmadik módosítás
+#jelentős késések kiválogatása és vizualizálása
+
+# Szűrés, rendezés, top 10 kiválasztása
+topkesok <- flights%>%
+  filter(dep_delay>120)%>%
+  arrange(desc(dep_delay))%>%
+  slice(1:10)
+
+# Vizualizáció: járatszám szerint tesszük
+ggplot(topkesok, aes(x=reorder(paste(origin, dest, sep=" → "), 
+                                   dep_delay), 
+                       y = dep_delay, fill=dep_delay)) +
+  geom_col(show.legend = FALSE) +
+  coord_flip() +
+  scale_fill_gradient(low = "orange", high = "red") +
+  labs(
+    title = "10 legnagyobb indulási késés",
+    x = "Útvonal (kiinduló → célállomás)",
+    y = "Indulási késés"
+  ) +
+  theme_minimal(base_size = 12) +
+  theme(
+    plot.title = element_text(face = "bold", hjust = 0.5),
+    axis.text = element_text(color = "gray20"),
+    axis.title = element_text(face = "bold")
+  )
